0131103074e085ad277c0f2b6b0e38afafdda806
Kim Steinhaug <kim@steinhaug.com>
2024-03-07 01:21:14 +0100

    Adding correct return status for processing page

--- htdocs/ajax.php
+++ htdocs/ajax.php
@@ -24,6 +24,17 @@ set_time_limit(0);
 if( !empty($_SESSION['task']['aiid']) ){
     require_once AJAX_FOLDER_PATH . '/openai/run-tasks.php';
 } else {
+    if( !empty($_GET['aiid'])){
+        $upload_id = (int) $_GET['aiid'];
+        $res = $mysqli->query("SELECT * FROM `" . $kista_dp . "uploaded_files` WHERE `upload_id`=" . $upload_id . " AND `user_id`=" . $USER_ID);
+        if ($res->num_rows) {
+            $item = $res->fetch_assoc();
+            if($item['status']=='error')
+                $_SESSION['error_msg'] = $item['error'];
+            echo json_encode(['status'=>$item['status'], 'progress'=>100]);
+            exit;
+        }
+    }
     //http_response_code(102);
     header('HTTP/1.0 200 OK');
     echo json_encode(['status'=>'idle','progress'=>0,'message'=>'Nothing to do.']);
