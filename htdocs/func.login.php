d73c2ed69632305a4f00da708f284c1f03b4429c
Kim Steinhaug <kim@steinhaug.com>
2024-03-07 23:17:07 +0100

    Updating paths

--- htdocs/func.login.php
+++ htdocs/func.login.php
@@ -2,12 +2,13 @@
 
 $USER_ID = 0;
 
+/*
 if( !isset($_COOKIE['PHPSESSID']) ){
     session_destroy();
     header("Location: reciepe.php?action=reset&return=" . rawurlencode(prepareLocation()));
     exit;
 }
-
+*/
 if( isset($_SESSION['USER_ID']) and isset($_SESSION['USER_SESSION']) ){
     $sql = "SELECT * 
             FROM `" . $kista_dp . "users__sessions` `us` 
@@ -19,11 +20,10 @@ if( isset($_SESSION['USER_ID']) and isset($_SESSION['USER_SESSION']) ){
         $_SESSION['logged_in'] = true;
         $USER_ID = $_SESSION['USER_ID'];
     } else {
-        header("Location: /login.php?action=login_error&return=" . rawurlencode(prepareLocation()));
+        header("Location: /auto-login.php?action=login_error&return=" . rawurlencode(prepareLocation()));
         exit;
     }
 } else {
-    header("Location: /login.php?action=login&return=" . rawurlencode(prepareLocation()));
+    header("Location: /auto-login.php?action=login&return=" . rawurlencode(prepareLocation()));
     exit;
-
 }
