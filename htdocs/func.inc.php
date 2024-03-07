f6f8f57b22d5e0d4334de2d9b8a9af3f324cd1d9
Kim Steinhaug <kim@steinhaug.com>
2024-03-07 17:06:04 +0100

    Adding lang titles

--- htdocs/func.inc.php
+++ htdocs/func.inc.php
@@ -53,12 +53,19 @@ function setActiveIf($x,$y){
     return '';
 }
 function HTML_FOOTER($i){
+    global $lang;
+
+    if($lang == 'nb')
+        $titles = ['Sider', 'Last opp', 'Galleri'];
+        else
+        $titles = ['Pages', 'Upload', 'Gallery'];
+
     return '
     <div id="footer-bar" class="footer-bar-6">
         <!-- <a href="index-components.html" class="' . setActiveIf($i,1) . '"><i class="fa fa-layer-group"></i><span>Features</span></a> -->
-        <a href="index.php" class="' . setActiveIf($i,2) . '"><i class="fa fa-file"></i><span>Pages</span></a>
-        <a href="upload.php" class="circle-nav ' . setActiveIf($i,3) . '"><i class="fa fa-home"></i><span>Last opp</span></a>
-        <a href="gallery.php" class="' . setActiveIf($i,4) . '"><i class="fa fa-camera"></i><span>Gallery</span></a>
+        <a href="index.php" class="' . setActiveIf($i,2) . '"><i class="fa fa-file"></i><span>' . $titles[0] . '</span></a>
+        <a href="upload.php" class="circle-nav ' . setActiveIf($i,3) . '"><i class="fa fa-home"></i><span>' . $titles[1] . '</span></a>
+        <a href="gallery.php" class="' . setActiveIf($i,4) . '"><i class="fa fa-camera"></i><span>' . $titles[2] . '</span></a>
         <!-- <a href="#" data-menu="menu-main" class="' . setActiveIf($i,5) . '"><i class="fa fa-bars"></i><span>Menu</span></a> -->
     </div>
     ';
