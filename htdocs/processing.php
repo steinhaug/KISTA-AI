bfa7ab2da25e815fe6a141252a12df39f5515743
Kim Steinhaug <kim@steinhaug.com>
2024-03-16 05:41:57 +0100

    Adding cache prevention for manifest

--- htdocs/processing.php
+++ htdocs/processing.php
@@ -29,7 +29,7 @@ if( empty($_GET['aiid']) ){
 <link rel="stylesheet" type="text/css" href="styles/style.css?<?=$html_NoCache_Version?>">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="fonts/css/fontawesome-all.min.css">
-<link rel="manifest" href="_manifest.json.php" data-pwa-version="<?=$PWA_APP_VER?>">
+<link rel="manifest" href="_manifest.json.php?<?=$PWA_APP_VER?>" data-pwa-version="<?=$PWA_APP_VER?>">
 <link rel="apple-touch-icon" sizes="180x180" href="app/icons/icon-192x192.png">
 </head>
 
