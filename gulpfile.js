61bd605b57654bace5325721ea4c626c3b6cada7
Kim Steinhaug <kim@steinhaug.com>
2024-03-21 01:47:21 +0100

    Adding gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,5 @@
 'use strict';
-
-const { src, dest, series, parallel, watch } = require('gulp');
-var debug = require('gulp-debug');
-var del                 = require('del');
-
 const { _compress } = require("./gulpfile-compress");
-
 
 async function _default(){
     console.log('  gulp compress');
