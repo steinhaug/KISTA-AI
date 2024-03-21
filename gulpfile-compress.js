f1d792132aaf0c52619eb528e8cb6debab6da62f
Kim Steinhaug <kim@steinhaug.com>
2024-03-21 01:46:42 +0100

    Adding compress logic

---
+++ gulpfile-compress.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const debug = require('gulp-debug');
+const del = require('del');
+
+/*
+import gulp from 'gulp';
+import uglify from 'gulp-uglify';
+import concat from 'gulp-concat';
+import debug from 'gulp-debug';
+import del from 'del';
+*/
+
+async function _compress () {
+
+    (async () => {
+        const deletedPaths = await del([
+            'scripts/gallery-controller.js'
+        ], {force: true, dryRun: false, onlyFiles:true});
+    })();
+
+    return gulp.src([
+            'htdocs/scripts/src/_functions/*.js'
+        ])
+        .pipe(debug())
+        .pipe(concat('gallery-controller.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('htdocs/scripts'));
+}
+
+module.exports = {
+  _compress
+}
